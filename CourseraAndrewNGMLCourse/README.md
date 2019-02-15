07761383d62e3685dd5bc2771c79915402fd7ad8
sharmilisrinivasan <sharmilisrinivasan@gmail.com>
2019-02-15 14:42:16 +0530

    Pushing Andrew NG ML Course exercises

---
+++ CourseraAndrewNGMLCourse/README.md
@@ -0,0 +1,1 @@
+Exercises solved in Coursera Andrew NG Machine Learning course (https://www.coursera.org/learn/machine-learning) - March 2015
---
+++ CourseraAndrewNGMLCourse/machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,32 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+    h_theta = X*theta;
+    error = h_theta-y; % size is (m*1)
+    to_be_subtracted = (error'*X)*(alpha/m); % size is (1*(n+1))
+    theta = theta - (to_be_subtracted');
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
