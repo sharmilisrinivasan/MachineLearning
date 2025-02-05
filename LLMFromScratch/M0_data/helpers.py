3a4d2a386234b09cc97ea48488b46bbb57462aad
Sharmili Srinivasan <sharmilisrinivasan@gmail.com>
2025-02-05 06:33:11 +0530

    Update tokenize_batch to handle string input. Add DataLoader & DataSet helpers

--- LLMFromScratch/M0_data/helpers.py
+++ LLMFromScratch/M0_data/helpers.py
@@ -1,22 +1,26 @@
 import tiktoken
 import torch
+from torch.utils.data import Dataset, DataLoader
 
 class GPT2Tokenizer():
     def __init__(self):
         self.tokenizer = tiktoken.get_encoding("gpt2")
 
-    def tokenize_batch(self, in_batch, max_in_seq_len):
-        #  in_batch is list of strings
+    def tokenize_batch(self, in_batch, max_in_seq_len=-1):
+        #  in_batch is list of strings or simple string
+        #  max_in_seq_len is used to resie out strings in case of multi length strings. Can be skipped for single string input.
         #  out_batch is Tensor list of Tensors of token_ids
+
+        if isinstance(in_batch, str):
+            in_batch = [in_batch]
 
         out_batch = []
 
         for in_str in in_batch:
-            # For Each String,
-            # 1. Encode using tokenizer
-            # 2. Convert to Tensor
-            # 3. Resize to max_in_seq_len: All inputs to be of same length = Either truncated or padded with 0s
-            out_batch.append(torch.tensor(self.tokenizer.encode(in_str)).resize_(max_in_seq_len))
+            to_append = torch.tensor(self.tokenizer.encode(in_str))  # Encode using tokenizer and Convert to Tensor
+            if max_in_seq_len != -1:
+                to_append = to_append.resize_(max_in_seq_len)  # 3. Resize to max_in_seq_len: All inputs to be of same length = Either truncated or padded with 0s
+            out_batch.append(to_append)
 
         return torch.stack(out_batch, dim=0)
 
@@ -29,3 +33,42 @@ class GPT2Tokenizer():
             out_batch.append(self.tokenizer.decode(data.tolist()))
 
         return out_batch
+
+class GPTDataset(Dataset):
+    def __init__(self, text, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenize
+        token_ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+
+        for i in range(0, len(token_ids)-max_length, stride):
+            input_ids_chunk = token_ids[i: i+max_length]
+            target_ids_chunk = token_ids[i+1: i+max_length+1]
+
+            self.input_ids.append(torch.tensor(input_ids_chunk))
+            self.target_ids.append(torch.tensor(target_ids_chunk))
+
+    def __len__(self):
+        return(len(self.input_ids))
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+# Method to load the Dataset class
+def create_data_loader(text, max_length=256, stride=128, batch_size=4, shuffle=True, drop_last = True, num_workers=0):
+
+    # Initialize tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    dataset = GPTDataset(text, tokenizer, max_length, stride)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
